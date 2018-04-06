34e0cd2d901c66856db591c9f4f903020a3e225d
mitsuaki kuwahara <quwahara@gmail.com>
2018-04-06 21:07:43 +0900

    Use trax

--- app/entity/design/new.php
+++ app/entity/design/new.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-  <script src="./js/trax/trax.js"></script>
+  <script src="../../../js/trax/trax.js"></script>
 </head>
 
 <body>
@@ -27,10 +27,41 @@ try {
     </div>
     <form>
       <div>Entity name</div>
-      <div><input type="text"></div>
-
-      <div>Field name</div>
       <div>
+        <input type="text">
+      </div>
+
+      <div>Fields</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="fields">
+              <td>
+                <input type="text" class="name">
+              </td>
+              <td>
+                <select class="value">
+                  <option>Text</option>
+                  <option>Multi line text</option>
+                  <option>Dropdown</option>
+                  <option>Seigle Checkbox</option>
+                  <option>Multi checkboxes</option>
+                  <option>Radio button</option>
+                </select>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+
+<!--
         <input type="text">
         <select>
         <option>Text</option>
@@ -40,14 +71,40 @@ try {
         <option>Multi checkboxes</option>
         <option>Radio button</option>
         </select>
-      </div>
+-->
 
-      <div><button type="button">Add field</button></div>
+      <div><button type="button" class="add-button">Add field</button></div>
 
       <div><button type="button">OK</button><button type="button">Cancel</button><button type="button">Clear</button></div>
 
     </form>
   </div>
+  <script>
+    var t;
+    t = new Trax({
+      name: "",
+      fields: [{
+        name: "",
+        type: "",
+      }],
+    });
+
+    // t.tx("apple", "#appleDiv .apple");
+    // t.rx("apple", "#appleText2@keyup");
+    // t.trx("apple", "#appleText");
+
+    t.tx("fields");
+
+    document.querySelector(".add-button")
+    .addEventListener("click", function (event) {
+      var field;
+      field = t.fields.createItem();
+      field.name = "";
+      field.type = "Text";
+      t.fields.push(field);
+    });
+  </script>
+
 </body>
 
 </html>
