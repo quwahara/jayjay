d1fae95b17f5201b877e627c5897add9ba091e1e
mitsuaki kuwahara <quwahara@gmail.com>
2019-01-05 18:52:37 +0900

    Change brx to booq

--- public/table-create-and-drop.php
+++ public/table-create-and-drop.php
@@ -31,7 +31,7 @@
     <link rel="stylesheet" type="text/css" href="css/fontawesome-free-5.5.0-web/css/all.css">
     <link rel="stylesheet" type="text/css" href="css/global.css">
     <script src="js/lib/node_modules/axios/dist/axios.js"></script>
-    <script src="js/brx/booq.js"></script>
+    <script src="js/booq/booq.js"></script>
     <script src="js/lib/global.js"></script>
     <?= '<style>' . $this->css()->style . '</style>' ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
